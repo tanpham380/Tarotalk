3af093b5eb0aba9a7ad1079b320b22fb8361df15
tanpham380 <phamthanhtanlop92@gmail.com>
2023-05-22 12:53:01 +0700

    updata html code

--- Tarot/views.py
+++ Tarot/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.views import View
+from django.views.static import serve
+
 # Create your views here.
 
 
 class TarotView(View):
     def get(self, request):
         return render(request, 'testpage.html')
+    def serve_static_file(request):
+        response = serve(request, 'css/styles.css')
+        response['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+        return response
