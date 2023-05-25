bff6e5e5e40e5d82d819678aac9ece229364ef87
tanpham380 <phamthanhtanlop92@gmail.com>
2023-05-25 21:14:02 +0700

    commit agian

--- Tarot/models.py
+++ Tarot/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import AbstractUser,Group, Permission
 # Create your models here.
 class User(AbstractUser):
     avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
+    name = models.CharField(max_length=255,  )
     groups = models.ManyToManyField(Group, related_name='tarot_users')
     user_permissions = models.ManyToManyField(Permission, related_name='tarot_users')
     
