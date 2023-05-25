c8fd4c105c8f9ae9475a8af940d11e128bbfcd54
tanpham380 <phamthanhtanlop92@gmail.com>
2023-05-25 22:17:29 +0700

    update models and admin.py

--- Tarot/models.py
+++ Tarot/models.py
@@ -2,12 +2,22 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser,Group, Permission
 # Create your models here.
 class User(AbstractUser):
-    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
+    avatar = models.ImageField(upload_to='static/avatars/', null=True, blank=True)
+    cover_page = models.ImageField(upload_to='static/cover_pages/', null=True, blank=True)
     name = models.CharField(max_length=255,  )
-    tarot_role = models.CharField(max_length=255, default= "Tarot Reading and AStrology")
-    status = models.CharField(max_length=255, default= "Active")
-    start_vote = models.IntegerField(default=0,  MaxValueValidator = 5, MinValueValidator = 0)
+    tarot_role = models.CharField(max_length=50, default= "Tarot Reading and AStrology")
+    introduction = models.TextField()
+    status = models.CharField(max_length=255)
+    start_vote = models.IntegerField()
     birth_date = models.DateField()
+    email = models.EmailField()
+    phone = models.CharField(max_length=10,  )
     groups = models.ManyToManyField(Group, related_name='tarot_users')
     user_permissions = models.ManyToManyField(Permission, related_name='tarot_users')
+class dich_vu (models.Model ):
+    id = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    name = models.CharField(max_length=255,  )
+    price = models.IntegerField()
+    content = models.TextField()
+    date  = models.DateField()
     
