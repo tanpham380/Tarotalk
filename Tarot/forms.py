2a118409d8fe83ba65c55864adb3eba92a4a4cab
tanpham380 <phamthanhtanlop92@gmail.com>
2023-06-07 22:15:25 +0700

    fix mer  html

--- Tarot/forms.py
+++ Tarot/forms.py
@@ -32,17 +32,25 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model=User
         fields = ['last_name','email','birth_date','email','phone_number','password1','password2']
-        
-class UpgradeUser(UserCreationForm):
+class UpgradeUserForm(UserCreationForm):
     introduction = forms.CharField(
         label='Giới thiệu',
-        widget=forms.Textarea(attrs={'class': 'form-control','required': 'required',}),
+        widget=forms.Textarea(attrs={'class': 'Upgradeform-control','required': 'required',}),
     )
     status = forms.CharField(
         label='Trạng thái',
-        widget=forms.TextInput(attrs={'class': 'form-control','required': 'required',}),
-    )    
+        widget=forms.TextInput(attrs={'class': 'Upgradeform-control','required': 'required',}),
+    )   
+    cover_page = forms.ImageField(
+        label='Ảnh bìa',
+        widget=forms.FileInput(attrs={'class': 'Upgradeform-control','required': 'required',}),
+    )
+    avatar = forms.ImageField(
+        label='Ảnh đại diện',
+        widget=forms.FileInput(attrs={'class': 'Upgradeform-control','required': 'required',}),
+    )
+     
     
     class Meta:
         model=User
-        fields = ['introduction','status']
+        fields = ['introduction','status','cover_page','avatar']
