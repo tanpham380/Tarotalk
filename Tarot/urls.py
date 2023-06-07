c504f4ee368062ee9b862735a31b45da8f54654a
tanpham380 <phamthanhtanlop92@gmail.com>
2023-06-07 21:20:43 +0700

    merge code

--- Tarot/urls.py
+++ Tarot/urls.py
@@ -2,6 +2,7 @@ import django.http
 from django.urls import path
 from . import views
 from django.views.generic.base import RedirectView
+from .views import *
 
 
 app_name = "Tarot"
@@ -12,13 +13,13 @@ urlpatterns = [
     path('register/', views.RegisterView.as_view(), name='register'),
     path('logout/', views.LogoutView, name='logout'),
     path('testpage/',  views.TestPageView.as_view(), name='HomePage'),
-    path("chatbot/", views.TarotView.chatbot, name="chatbot"),
-    path("list/", views.TarotView.listReader, name="list"),
-    path("profile/", views.TarotView.profile, name="profile"),
-    path("questions/", views.TarotView.question, name="questions"),
-    path("package/", views.TarotView.package, name="package"),
-    path("hour/", views.TarotView.hour, name="hour"),
-    path("calendar/choose/", views.TarotView.chooseSlot, name="choose"),
-    path("calendar/choose/checkout/", views.TarotView.checkout, name="checkout"),
-    path("calendar/", views.TarotView.calendar, name="calendar"),
+    path("list/", listReader, name="list"),
+    path("profile/", profile, name="profile"),
+    path("questions/", question, name="questions"),
+    path("chatbot/", chatbot, name="chatbot"),
+    path("package/", package, name="package"),
+    path("hour/", hour, name="hour"),
+    path("calendar/choose/", chooseSlot, name="choose"),
+    path("calendar/choose/checkout/", checkout, name="checkout"),
+    path("calendar/", calendar, name="calendar"),
 ]
