4a32a34424c7ea463557f2d17ae3f414e1d52a1b
tanpham380 <phamthanhtanlop92@gmail.com>
2023-06-09 16:57:46 +0700

    add static

--- Tarot/urls.py
+++ Tarot/urls.py
@@ -3,8 +3,7 @@ from django.urls import path
 from . import views
 from django.views.generic.base import RedirectView
 from .views import *
-from django.conf.urls.static import static
-from django.conf import settings
+
 
 
 app_name = "Tarot"
